dfebc82955a73fbe301392c57aacc0ea7fdbf662
Rauf Kurbanov <kurbanov.re@gmail.com>
2021-07-11 10:21:24 +0300

    flagging damaged images in metadata

--- augmentations.py
+++ augmentations.py
@@ -16,6 +16,18 @@ def train_no_aug_crop() -> A.Compose:
     return train_transform
 
 
+def train_crop_flip() -> A.Compose:
+    train_transform = A.Compose([
+        A.RandomCrop(height=512, width=512),
+        A.Resize(height=224, width=224),
+        A.Flip(p=0.5),
+        A.Normalize(mean=MEAN, std=STD),
+        ToTensorV2(),
+    ])
+
+    return train_transform
+
+
 def train_no_aug_resize() -> A.Compose:
     train_transform = A.Compose([
         A.Resize(height=224, width=224),
@@ -40,6 +52,6 @@ AUGS_SELECT = {
     "no_aug_crop": train_no_aug_crop,
     "no_aug_resize": train_no_aug_resize,
     "flip": train_flip,
+    "crop_flip": train_crop_flip,
 }
-
 
